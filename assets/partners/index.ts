2addbc8cce128e19823e597a782dd6086e825b5e
fatra <alkhawarizmifatra@gmail.com>
2025-12-23 10:12:30 +0700

    Add: Partner Logos

--- assets/partners/index.ts
+++ assets/partners/index.ts
@@ -5,6 +5,25 @@ import AupairMaria from "./aupair-maria.webp";
 import FairPair from "./fair-pair.webp";
 import MultiKultur from "./multi-kultur.webp";
 import Zubke from "./zubke.webp";
+import AgentAupair from "./agent-aupair.webp";
+import AleduSprache from "./aledu-sprache.webp";
+import ApituGoExplore from "./apitu-goexplore.webp";
+import AupairBerlinde from "./aupair-berlinde.webp";
+import AupariClick from "./aupair-click.webp";
+import AupairConnect from "./aupair-connect.webp";
+import AupairConnexion from "./aupair-connexion.webp"
+import AupairOffice from "./aupair-office.webp"
+import AupairSociety from "./aupair-society.webp"
+import AupairVermittlung from "./aupair-vermittlung.webp"
+import AzubiSolution from "./azubi-solution.webp"
+import BeriGermany from "./beri-germany.webp"
+import EuropairService from "./europair-service.webp"
+import FeeRevee from "./fee-revee.webp"
+import Kulturist from "./kulturist.webp"
+import LesEnfants from "./les-enfants.webp"
+import PmeFamilien from "./pme-familien.webp"
+import SolutionEt from "./solution-et.webp"
+
 
 export {
      AgenturTaunus,
@@ -14,4 +33,22 @@ export {
      FairPair,
      MultiKultur,
      Zubke,
+     AgentAupair,
+     AleduSprache,
+     ApituGoExplore,
+     AupairBerlinde,
+     AupariClick,
+     AupairConnect,
+     AupairConnexion,
+     AupairOffice,
+     AupairSociety,
+     AupairVermittlung,
+     AzubiSolution,
+     BeriGermany,
+     EuropairService,
+     FeeRevee,
+     Kulturist,
+     LesEnfants,
+     PmeFamilien,
+     SolutionEt,
 };
